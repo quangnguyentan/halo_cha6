c403655400e0f55e308c176fa0dc4e4dda74b50a
unknown <tanquanga6k10@gmail.com>
2024-09-24 00:26:23 +0700

    update code

--- app/(root)/chats/page.jsx
+++ app/(root)/chats/page.jsx
@@ -1,6 +1,5 @@
 import ChatList from "@components/ChatList";
 import Contacts from "@components/Contacts";
-import AppProviderBasic from "@components/DashBoard";
 
 const Chats = () => {
   return (
@@ -8,7 +7,6 @@ const Chats = () => {
       {/* <div className="w-1/3 max-lg:w-1/2 max-md:w-full">
         <ChatList />
       </div> */}
-      {/* <AppProviderBasic/> */}
       <div className="w-1/3 max-lg:w-full max-md:hidden flex-col  ">
         <Contacts />
       </div>
